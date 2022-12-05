fdcb7faf55040e8e0000fda941b434193da21640
aslanliIbrahim <ibrahimra@code.edu.az>
2022-12-05 14:55:03 +0400

    Section two is ready for use

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const crslContent = document.querySelectorAll(".caruesel-content");
+
+const cardAnimatie = [
+    {transform: "scale(1)"},
+    {transform: "scale(1.02)"},
+    {transform: "scale(1.03)"},
+    {transform: "scale(1.04)"}
+]
+
+const cardanimation = {
+    duration:10000,
+    iterations:1
+}
+
+for (let i = 0; i < crslContent.length; i++) {
+
+    const currentContent = document.querySelectorAll(".caruesel-content.animation-class")
+    
+    if (crslContent[i].dataset.target === "1") {
+        crslContent[i].addEventListener("mouseover", function(){
+            crslContent[i].animate(cardAnimatie,cardanimation)
+        })
+        
+    } else if(crslContent[i].dataset.target === "2") {
+        crslContent[i].addEventListener("mouseover", function(){
+            crslContent[i].animate(cardAnimatie,cardanimation)
+        })
+    } else if(crslContent[i].dataset.target === "3"){
+        crslContent[i].addEventListener("mouseover", function(){
+            crslContent[i].animate(cardAnimatie,cardanimation)
+        })
+    } else if(crslContent[i].dataset.target === "4"){
+        crslContent[i].addEventListener("mouseover", function(){
+            crslContent[i].animate(cardAnimatie,cardanimation)
+        })
+    }
+
+}
